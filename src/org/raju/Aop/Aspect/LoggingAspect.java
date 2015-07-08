1c6c2310329fd218f396d78e8f22738be0516cef
kittu <kitturaju5@gmail.com>
2015-07-08 12:01:11 -0400

    more code added

--- src/org/raju/Aop/Aspect/LoggingAspect.java
+++ src/org/raju/Aop/Aspect/LoggingAspect.java
@@ -8,6 +8,7 @@ public class LoggingAspect {
 	@Before("execution(public String org.raju.Aop.model.Circle.getName())")
 	public void LoggingAdvice(){
 		System.out.println("Print this message...");
+		//I add this line as a checkup
 	}
 
 }
