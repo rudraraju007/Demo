fa563d1ae89b68b1415e156606ba52e19c93c75a
kittu <kitturaju5@gmail.com>
2015-07-08 11:20:58 -0400

    initial commit

---
+++ src/org/raju/Aop/AopMain.java
@@ -0,0 +1,16 @@
+package org.raju.Aop;
+
+import org.raju.Aop.service.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopMain {
+
+	public static void main(String[] args) {
+
+ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+ShapeService shapeService= context.getBean("shapeService",ShapeService.class);
+System.out.println(shapeService.getTriangle().getName());
+	}
+
+}
---
+++ src/org/raju/Aop/Aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package org.raju.Aop.Aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+	@Before("execution(public String org.raju.Aop.model.Circle.getName())")
+	public void LoggingAdvice(){
+		System.out.println("Print this message...");
+	}
+
+}
